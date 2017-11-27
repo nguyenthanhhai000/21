b54828809f58abe1139ce7daf94926d57fded8e3
nguyenthanhhai000 <32790760+nguyenthanhhai000@users.noreply.github.com>
2017-11-27 07:09:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
             $urlRouterProvider
                 .otherwise(function ($injector) {
                     var $state = $injector.get('$state');
-                    $state.go('index.control');
+                    $state.go('index.home');
                 })
         });
 
